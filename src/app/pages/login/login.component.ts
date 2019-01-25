672cd4a9f081b0a4177f5732b4de7e4d1117e9de
trinhtc <trinhtc@mti-tech.vn>
2019-01-25 17:06:06 +0700

    Fix build error on prod

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,12 +3,15 @@ import { AuthorizeService } from 'src/app/services/authorize.service';
 import { HttpResponse } from 'src/app/models/HttpResponse';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username:string;
+  password: string;
   error = '';
   block = false;
   constructor(
