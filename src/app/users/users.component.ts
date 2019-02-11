e2dae2fc4e12f007938318aa00a41f9b84810ac9
trinhtc <trinhtc@mti-tech.vn>
2019-02-11 16:55:25 +0700

    Fix loading forever when api errors

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -27,8 +27,10 @@ export class UsersComponent implements OnInit {
     this.blockUiService.blockUi();
     this.userService.getUsers().subscribe(users=>{ 
         this.users = users;
-        this.blockUiService.unBlockUi(); 
+      },err =>{
+        console.log('Error Getting Users');  
       });
+      this.blockUiService.unBlockUi(); 
   }
 
   ngOnInit() {
