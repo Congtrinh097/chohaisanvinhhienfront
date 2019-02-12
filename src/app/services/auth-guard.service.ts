442e26589efd705055ea7b7b1e62ec93d5ce8ea3
trinhtc <trinhtc@mti-tech.vn>
2019-02-12 15:29:49 +0700

    remote reduntdan code

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { Location } from '@angular/common';
 import { AuthService } from './auth.service';
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  constructor( private location: Location, public router: Router) {}
+  constructor( public router: Router) {}
   canActivate(): boolean {
     if (!AuthService.isAuthenticated()) {
       this.router.navigate(['login']);
